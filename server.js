d5f57e6d9e698ce71b3bcea07104b2ad7eb5c726
cunningham-samuel <cunningham.samuel@gmail.com>
2025-06-05 22:06:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 
